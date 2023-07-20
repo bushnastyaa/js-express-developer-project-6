d2455c50d93d4d14f3914cac4d77b92d443d5b25
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-07-20 19:25:05 +0300

    Add tasks tests

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -15,6 +15,7 @@ export const prepareData = async (app) => {
 
   await knex('users').insert(getFixtureData('users.json'));
   await knex('statuses').insert(getFixtureData('statuses.json'));
+  await knex('tasks').insert(getFixtureData('tasks.json'));
 };
 
 export const getCookie = async (app, testData) => {
